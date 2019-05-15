ca871854bc01759bebeeaf492ceb33da2867e496
hashdhahsdhas <50675293+hashdhahsdhas@users.noreply.github.com>
2019-05-15 19:50:14 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+ 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`   ||   `,'https://www.twitch.tv/zya2d_')
+    client.user.setStatus('ldle');
+});
+
+client.on("message", message => {
+
+const prefix = '%'
+ 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
+
+
+
+ client.login(process.env.BOT_TOKEN); 
