d968d77216d97488532cce20253bc6a86a7e0ea8
hashdhahsdhas <50675293+hashdhahsdhas@users.noreply.github.com>
2019-05-15 20:02:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 client.on("message", message => {
 
-const prefix = '%'
+const prefix = '-'
  
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
@@ -23,7 +23,22 @@ const prefix = '%'
  message.delete();
 };    
 });
+ 
 
+client.on("message", message => {
+ 
+ const prefix = '-'
 
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
 
- client.login(process.env.BOT_TOKEN); 
+client.login(process.env.BOT_TOKEN);
